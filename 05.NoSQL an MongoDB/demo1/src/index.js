d08db0ecd9034d4fa0f675ed4830d38b8a3acf38
MartinKochanov <114015998+MartinKochanov@users.noreply.github.com>
2024-05-29 00:50:46 +0200

    some experiments

--- 05.NoSQL an MongoDB/demo1/src/index.js
+++ 05.NoSQL an MongoDB/demo1/src/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const { Person } = require('./models/Person')
+const { Person } = require('./models/Person');
+const { Article } = require('./models/Article');
 
 async function start() {
     const connectioString = 'mongodb://localhost:27017/testdb';
@@ -7,8 +8,27 @@ async function start() {
 
     console.log('Database connected');
 
-    const bob = await Person.findOne({firstName: 'Bob'});
-    console.log(bob);
+    // const bob = await Person.findOne({ firstName: 'Bob' });
+
+    // await Article.create({
+    //     content: 'First article',
+    //     author: bob
+    // })
+
+    // try {
+    //     const result = await Person.findOneAndDelete().where('firstName').equals('Bob');
+    //     console.log(result);
+    // } catch (err) {
+    //     console.log(err);
+    // }
+
+    
+    try {
+        const result = await Person.deleteOne().where('firstName').equals('Bob');
+        console.log(result);
+    } catch (err) {
+        console.log(err);
+    }
     mongoose.disconnect();
 }
 
